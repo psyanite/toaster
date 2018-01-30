8db02cdd843e3113b6baf9c69af1a9ec898a1b98
Unknown <psyanite@gmail.com>
2018-01-30 22:15:36 +1100

    Update schema to reference queries and mutations

--- src/data/schema.js
+++ src/data/schema.js
@@ -1,28 +1,10 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-import {
-  GraphQLSchema as Schema,
-  GraphQLObjectType as ObjectType,
-} from 'graphql';
-
-import me from './queries/me';
-import news from './queries/news';
+import { GraphQLSchema as Schema } from 'graphql';
+import Query from './queries';
+import Mutation from './mutations';
 
 const schema = new Schema({
-  query: new ObjectType({
-    name: 'Query',
-    fields: {
-      me,
-      news,
-    },
-  }),
+  query: Query,
+  mutation: Mutation,
 });
 
 export default schema;
