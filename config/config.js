94d13ffd92c1c0aa23ace092cfce0788f9c8dfba
Unknown <psyanite@gmail.com>
2018-01-30 22:10:48 +1100

    Add config file for dotenv

---
+++ config/config.js
@@ -0,0 +1,70 @@
+/* eslint-disable max-len,global-require */
+
+if (process.env.BROWSER) {
+  throw new Error(
+    'Do not import `config.js` from inside the client-side code.',
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+module.exports = {
+  // Node.js app
+  port: process.env.PORT || 3000,
+
+  // API Gateway
+  api: {
+    // API URL to be used in the client-side code
+    clientUrl: process.env.API_CLIENT_URL || '',
+    // API URL to be used in the server-side code
+    serverUrl:
+      process.env.API_SERVER_URL ||
+      `http://localhost:${process.env.PORT || 3000}`,
+  },
+
+  // Database
+  database: {
+    name: process.env.DATABASE_NAME || 'public',
+    dialect: process.env.DATABASE_DIALECT || 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || 5432,
+    username: process.env.DATABASE_USERNAME || 'username',
+    password: process.env.DATABASE_PASSWORD || 'password',
+  },
+
+  // Web analytics
+  analytics: {
+    // https://analytics.google.com/
+    googleTrackingId: process.env.GOOGLE_TRACKING_ID, // UA-XXXXX-X
+  },
+
+  // Authentication
+  auth: {
+    jwt: { secret: process.env.JWT_SECRET || 'JWT Secret' },
+
+    // https://developers.facebook.com/
+    facebook: {
+      id: process.env.FACEBOOK_APP_ID || '186244551745631',
+      secret:
+        process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+    },
+
+    // https://cloud.google.com/console/project
+    google: {
+      id:
+        process.env.GOOGLE_CLIENT_ID ||
+        '251410730550-ahcg0ou5mgfhl8hlui1urru7jn5s12km.apps.googleusercontent.com',
+      secret: process.env.GOOGLE_CLIENT_SECRET || 'Y8yR9yZAhm9jQ8FKAL8QIEcd',
+    },
+
+    // https://apps.twitter.com/
+    twitter: {
+      key: process.env.TWITTER_CONSUMER_KEY || 'Ie20AZvLJI2lQD5Dsgxgjauns',
+      secret:
+        process.env.TWITTER_CONSUMER_SECRET ||
+        'KTZ6cxoKnEakQCeSpZlaUCJWGAlTEBJj0y2EMkUBujA7zWSvaQ',
+    },
+  },
+};
