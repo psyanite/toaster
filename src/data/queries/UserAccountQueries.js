c9caf2bd22eb12cb46b9aac62a98c02fe74247dc
Unknown <psyanite@gmail.com>
2018-01-30 22:15:02 +1100

    Add user account queries

---
+++ src/data/queries/UserAccountQueries.js
@@ -0,0 +1,27 @@
+import {
+  GraphQLList as List,
+  GraphQLNonNull as NonNull,
+  GraphQLInt as Int,
+} from 'graphql'
+import { resolver } from 'graphql-sequelize'
+import { UserAccountType } from '../types'
+import { UserAccount } from '../models'
+
+export default {
+  allUserAccounts: {
+    type: new List(UserAccountType),
+    resolve() {
+      return UserAccount.findAll({}).then(data => data)
+    },
+  },
+
+  userAccountById: {
+    type: new List(UserAccountType),
+    args: {
+      id: {
+        type: new NonNull(Int),
+      },
+    },
+    resolve: resolver(UserAccount),
+  },
+}
