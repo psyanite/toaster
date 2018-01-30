51bbc811e2946c4eccb2a3ff03121387dd021479
Unknown <psyanite@gmail.com>
2018-01-30 22:14:55 +1100

    Add post queries

---
+++ src/data/queries/PostQueries.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-param-reassign */
+import {
+  GraphQLList as List,
+  GraphQLNonNull as NonNull,
+  GraphQLInt as Int,
+} from 'graphql'
+
+import { resolver } from 'graphql-sequelize'
+import { PostType } from '../types'
+import { Post } from '../models'
+
+export default {
+  allPosts: {
+    type: new List(PostType),
+    resolve() {
+      return Post.findAll({}).then(data => data)
+    },
+  },
+
+  postById: {
+    type: new List(PostType),
+    args: {
+      id: {
+        type: new NonNull(Int),
+      },
+    },
+    resolve: resolver(Post),
+  },
+
+  postsByStoreId: {
+    type: new List(PostType),
+    args: {
+      storeId: {
+        type: new NonNull(Int),
+      },
+    },
+    resolve: resolver(Post, {
+      before: (findOptions, args) => {
+        findOptions.where = {
+          store_id: args.storeId,
+        }
+        findOptions.order = [['posted_at', 'ASC']]
+        return findOptions
+      },
+    }),
+  },
+}
