35dce4083c1d4a1302fdcd02f3a02a3c6c407c06
Unknown <psyanite@gmail.com>
2018-07-08 17:03:37 +1000

    Update runServer for debug

--- tools/runServer.js
+++ tools/runServer.js
@@ -45,7 +45,7 @@ function runServer() {
       server.kill('SIGTERM');
     }
 
-    server = cp.spawn('node', [serverPath], {
+    server = cp.spawn('node', ['--debug --debug-brk', serverPath], {
       env: Object.assign({ NODE_ENV: 'development' }, process.env),
       silent: false,
     });
