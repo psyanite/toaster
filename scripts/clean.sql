88dc6d419808d29252b964f6a6e20ebe09fc96cc
Nyatella Toast <psyanite@gmail.com>
2020-04-25 13:52:12 +1000

    Update scripts

--- scripts/clean.sql
+++
@@ -1,9 +0,0 @@
-create database burntoast;
-
-use schema public;
-CREATE EXTENSION unaccent;
-create extension cube;
-create extension earthdistance;
-create extension unaccent;
-
-create schema croissant;
