2c8f2efff78024db685f7adbbc4495b6075cee7d
Nyatella Toast <psyanite@gmail.com>
2020-05-20 21:03:42 +1000

    Add curl script

---
+++ docs/scripts/curl/run2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 1. Update cat to filename
+# 2. `./run.sh`
+
+for i in $(cat stores2.csv)
+do
+  arrIN=(${i//,/ })
+  url=${arrIN[0]}
+  filename=${arrIN[1]}
+
+  curl -o $(echo $filename | sed 's/\?.*//') $(echo $url | tr '\r' ' ')
+
+  unset arrIN
+  unset url
+  unset filename
+done
